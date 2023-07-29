5a3c7299fdddacdb38ca6847f90b9e94ff611e87
Williams Salazar <93990745+williamsjdev@users.noreply.github.com>
2023-07-29 14:36:24 -0400

    deployment

---
+++ models/Paciente.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const pacientesSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        require: true
+    },
+    propietario: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    fecha: {
+        type: Date,
+        require: true,
+        default: Date.now(),
+    },
+    sintomas: {
+        type: String,
+        require: true
+    },
+    veterinario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Veterinario"
+    },
+}, {
+    timesTamps: true,
+});
+
+const Paciente = mongoose.model('Paciente', pacientesSchema);
+
+export default Paciente;
